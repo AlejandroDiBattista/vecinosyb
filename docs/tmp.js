8fac7063f7bebd04a5f212989bb194db6a46a9ba
Alejandro Di Battista <alejandrodibattista@gmail.com>
2020-11-14 00:29:47 -0300

    .

---
+++ docs/tmp.js
@@ -0,0 +1,12 @@
+// const options = { 
+	// 	includeScore: 0, 
+	// 	keys: ['comercio', 'domicilio', 'rubro'],
+	// }
+
+	// let fuse = new Fuse(this.origen, options);
+	// this.comercios = fuse.search("solano vera");
+	// this.comercios = this.comercios.map(x => x.item).sort( x => x.rubro);
+
+	// console.log("Comercios...");
+	// this.rubros = agruparPorRubros( this.comercios );
+	// console.log(this.rubros);
